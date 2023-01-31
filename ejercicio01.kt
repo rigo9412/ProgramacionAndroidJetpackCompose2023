a1d2200acc4c8f5d4206fc15bae4d596bff974c3
JaimeVJ-TEC <l19100265@nlaredo.tecnm.mx>
2023-01-31 10:04:59 -0600

    ejercicio1

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,22 @@
 //Elaborar una función que reciba una cadena de texto y la invierta.
 
+fun main() {
+    println(ReverseString("Hola mundo"))
+    println(ReverseString(null))
+}
+
+fun ReverseString(str1 : String?) : String?{
+    if(str1 == null) {
+        return "nulo"
+    }
+    
+    var strLen = str1.length
+    var reversedStr = ""
+    
+    while(strLen > 0){
+    	reversedStr = reversedStr + str1[strLen-1] 
+        strLen -= 1
+    }
+    
+    return reversedStr
+}
