3bb32adf0409237960b2d5b46c4a7f37f0d9465f
AlMy 3 <70553666+Aldo-Ezequiel-Rodriguez-Mendez@users.noreply.github.com>
2023-01-31 13:36:29 -0600

    txt

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,17 @@
 //Elaborar una función que reciba una cadena de texto y la invierta.
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    stringInvert("Hello, world!!!")
+}
 
+fun stringInvert(value: String? = "default"){
+    if (value == null) return;
+    
+    for(item in value.lastIndex downTo 0){
+        print(value[item])
+    }
+    
+}
