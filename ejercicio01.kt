17e764c22262c10a2a96689cefa243ba909b9862
Alan Castro <alan.ssuc2311@gmail.com>
2023-01-31 09:54:54 -0600

    Ejercicio 1

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,21 @@
-//Elaborar una función que reciba una cadena de texto y la invierta.
+////EJERCICIO 1
+////Elaborar una función que reciba una cadena de texto y la invierta.
 
+
+fun invertString(cad: String): String {
+    var cont = cad.length - 1
+    var invertido = ""
+    
+    while(cont >= 0) {
+        invertido += cad.get(cont)
+        cont -= 1
+    }    	
+        
+    return invertido
+}
+
+
+fun main() {
+    var inv = invertString("HOLA ESTA ES UNA PRUEBA")
+    println(inv)
+}
