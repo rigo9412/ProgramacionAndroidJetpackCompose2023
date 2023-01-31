bdf45e843bdcb2491a62e6ccec8a449dafb9ab9c
Eduardo Jared Aguirre Cardenas <ejaredac@hotmail.com>
2023-01-31 09:49:24 -0600

    Ejercicio 1 - 19100140

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,17 @@
-//Elaborar una función que reciba una cadena de texto y la invierta.
+//Ejercicio 1 - 19100140
+////Elaborar una función que reciba una cadena de texto y la invierta.
+fun main() {
+    
+    println(Invertir("Hola"))
+}
 
+fun InvertirTexto(cadenas : String? = "defult") : String {
+    var strCadenaRecibida : String = cadenas
+    var strInv = ""
+    for (i in strCadenaRecibida.lastIndex downTo 0){
+        strInv = strInv + strCadenaRecibida[i]
+    }
+    
+	return (strCadenaRecibida.reversed())
+    
+}
