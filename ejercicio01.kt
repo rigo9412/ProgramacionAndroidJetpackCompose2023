8d333348d9a6c48b4b708d63f966bccc1c36052c
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-01-31 23:17:20 -0600

    Ejercicios 1 y 2

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,26 @@
 //Elaborar una función que reciba una cadena de texto y la invierta.
 
+fun main() {
+    println(stringInvert("Hello,world!!!"));
+    println(ReverseString("Jordan Yo"))
+}
+
+fun stringInvert(value:String):String{
+    if (value==null) return "Cadena vacia";
+    var newString:String=""
+    for(item in value.length-1 downTo 0){
+       newString+=value[item]
+    }
+    return newString
+}
+
+
+fun ReverseString(value:String):String{
+    if (value==null) return "Cadena vacia";
+    var newString:String=""
+    var i:Int = value.lastIndex+1
+    while(--i >=0){
+        newString+=value[i]
+    }
+    return newString
+}
