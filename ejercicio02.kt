1675d6554d66f01bd2abf003d3d77c7ee1a0f931
JaimeVJ-TEC <l19100265@nlaredo.tecnm.mx>
2023-02-01 09:28:17 -0600

    cambios a ejercicio 2

--- ejercicio02.kt
+++ ejercicio02.kt
@@ -2,6 +2,7 @@
 
 fun main() {
     println(CamelString("Esto es una Cadena"))
+    println(CamelString("Hola     MUNdo-Kotlin_Android-Jet_Pack"))
     println(CamelString(null))
 }
 
@@ -9,14 +10,16 @@ fun CamelString(str1 : String?) : String?{
     if(str1 == null) {
         return "nulo"
     }
-    
     var strCounter = 0
     var convertedString = ""
-
     while(strCounter < str1.length){
         if(str1[strCounter] == ' ' || str1[strCounter] == '-'  || str1[strCounter] == '_'){
-            convertedString = convertedString + str1[strCounter+1].uppercase()
-            strCounter += 2
+            if(str1[strCounter + 1].isLetter()){
+                convertedString = convertedString + str1[strCounter+1].uppercase()
+                strCounter += 2
+                continue
+            }
+            strCounter += 1
             continue
         }
         else{
@@ -24,7 +27,6 @@ fun CamelString(str1 : String?) : String?{
             strCounter += 1
         }
     }
-    
     convertedString = convertedString.replaceFirstChar{it.lowercase()}
     return convertedString
 }
