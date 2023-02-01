c8b2872973e9f04d84c2caf51d293d0a31a10e94
Alan Castro <70525663+AeaX2311@users.noreply.github.com>
2023-02-01 09:41:51 -0600

    Update ejercicio02.kt

--- ejercicio02.kt
+++ ejercicio02.kt
@@ -1,14 +1,18 @@
-fun camelCase(palabra: String) :String {
-    var camel = ""
-    var cont = 0    
+fun camelCase(palabra: String) :String {        
+    var camel:String = ""
+    var cont = 1    
     var bandera: Boolean = false
-    while(cont < palabra.length) {
-        var letra = palabra.get(cont)
+    var tamanio = palabra.length
+    
+    camel += palabra[0].toLowerCase()
+    
+    while(cont < tamanio) {
+        var letra = palabra[cont]
         
-        if(letra != ' ' && letra != '-' && letra != '_' && letra != '.' && letra != ',' && letra != ';') {
+        if(letra != ' ' && letra != '-' && letra != '_' && letra != '.'&& letra != ',' && letra != ';') {
            if(bandera) { //Es la primera
                camel += letra.toUpperCase()
-           } else { //Es mayuscula
+           } else {
                camel += letra.toLowerCase()
            }
            
