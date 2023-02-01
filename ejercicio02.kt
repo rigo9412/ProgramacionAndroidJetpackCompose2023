f42d0be4e8d0ad663d9dd883a22533bb815dabf0
AlMy 3 <70553666+Aldo-Ezequiel-Rodriguez-Mendez@users.noreply.github.com>
2023-01-31 22:22:29 -0600

    ejercicio2

---
+++ ejercicio02.kt
@@ -0,0 +1,27 @@
+// Elaborar una función que convierta un texto de camelCase 
+// Ejemplo : "Esto es una Cadena" -> "estoEsUnaCadena"
+
+fun main() {
+    println(stringConvertCamelCase("Esto es una nueva cadena"))
+}
+
+fun stringConvertCamelCase(miCadena: String): String?{
+
+    var cadenaCamelCase = "" 
+    val palabras = miCadena.split("\\s".toRegex()).toTypedArray() //Guardamos las palabras en un arreglo
+    //println(palabras.contentToString())
+
+    val palabraSinMayuscula = palabras.first() //Obtenemos la primera palabra que no debe ser en mayusculas
+    //println(palabraSinMayuscula)
+
+    val palabrasConMayuscula = palabras.drop(1) //Guardamos en otro arreglo solo las palabras que tendran mayusculas
+    //println(palabrasConMayuscula)
+
+    //Agregamos primero la palabra que no tiene mayusculas en la variable auxiliar
+    cadenaCamelCase = cadenaCamelCase + palabraSinMayuscula.toLowerCase() 
+
+    for(palabra in palabrasConMayuscula){ 
+        cadenaCamelCase = cadenaCamelCase + palabra.capitalize() //Agregamos todas las palabras que van en mayusculas
+    }
+    return(cadenaCamelCase)
+}
