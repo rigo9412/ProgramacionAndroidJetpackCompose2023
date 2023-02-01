941150ed335bb5e47a1bbcc6dd786eec9145de7e
¨RevisOTO¨ <rafamosqueda.barragan12@gmail.com>
2023-01-31 20:14:18 -0600

    Tarea 1 Sting Invertido

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,23 @@
 //Elaborar una función que reciba una cadena de texto y la invierta.
 
+fun main() {
+  strInvert("Hola")
+}
+
+fun stringInvert(value: String? = "default") {
+
+  if(value == null) return;
+  for(item in value.lastIndex downTo 0){
+    print(value[item])
+  }
+}
+
+fun strInvert(value: String? = "default"){
+    if(value == null) return;
+ 
+    var i = value.lastIndex;
+	while( i >= 0 ){
+        print(value[i])
+        i = i-1
+    }
+}
