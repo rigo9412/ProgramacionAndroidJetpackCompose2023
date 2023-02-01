d4940ae0fe69d7ac241e73634fdd3567bf59b26f
GemaMeraz <gema_nataly05@hotmail.com>
2023-02-01 08:15:58 -0600

    Ejercicio01

--- ejercicio01.kt
+++ ejercicio01.kt
@@ -1,2 +1,20 @@
 //Elaborar una función que reciba una cadena de texto y la invierta.
 
+fun main() {
+    stringInvert("Hello, world!!!")
+    cadenaInversa("cadena es un tipo de dato")
+}
+
+/*método visto en clase*/
+fun stringInvert(value: String?) {
+    if(value==null) return;
+    
+    for(item in value.lastIndex downTo 0){
+        print(value[item])
+    }
+}
+
+
+fun cadenaInversa(value:String?){
+    if(value==null) return;
+    print("\n"+value.reversed())
