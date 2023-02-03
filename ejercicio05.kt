7d01a6d061a58363a4bf9fca2df322a35b235489
Alan Peña Ortiz <alancin7@live.com.mx>
2023-02-03 09:14:29 -0600

    code: quitar llaves del mapa

--- ejercicio05.kt
+++ ejercicio05.kt
@@ -73,5 +73,5 @@ fun main() {
             "Cupio earumque tranquilli viderer? ángoribus cognita duxit fáciendumve, finiri habémus hae impetu intemperantes lógikh novum quaerimus sadipscing virtus? ántipátrum cursus elegáns interesse ius, perpetua stulti! Assecutus concludaturque legimus metum primo quaestionem senserit splendido? Albuci apte atqui controversia difficilius, habeatur modice numquam rogatiuncula. \n" +
             " \n" +
             "Derigatur ignoratione tuemur umbram verum! áfferát comparat dicitis neque omnisque, praeteritas satis sólidó? Augue dapibus duo eruditi, finitas huic invidi maestitiam mererer nemo scribere splendore? ádipiscing aliquip curáe discordant, essent gravissimas intellegi maluisset offendit perspici se solido tria véra voluit. Accommodare claudicare copiosae fuit, homini loca oratione seiunctum, situm superstitió veniam voluntátes! Disciplinis disserui ergo harum id imitarentur multos mundus nostrud poenis relinqueret tollitur! "
-    println(contar(texto).toSortedMap())
+println(contar(texto).toSortedMap().toString().replace("[\\s{|\\s}]".toRegex(), ""))
 }
