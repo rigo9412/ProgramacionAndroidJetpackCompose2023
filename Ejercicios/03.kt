e524575a294d510aaac5e02e2d1cc26d0604b6b8
Alan Castro <70525663+AeaX2311@users.noreply.github.com>
2023-02-03 09:39:00 -0600

    Ejercico 3 correcto

--- Ejercicios/03.kt
+++ Ejercicios/03.kt
@@ -1,30 +1,10 @@
 ////EJERCICIO 3
 ////Elaborar una función que reciba un texto y cuente el numero de letras que tiene
-import java.text.Normalizer
-
 fun main() {
-    println(calcularNumeroLetras("aaaaabbbbcccccccdddddd Hola me Llamo AlaéáéíóůÃÇÕÊeÚclairn".unaccent().uppercase()))
+    var cadee =  "abc234567876543234567876.5/.45?>%&$%^&#@$^#$%@43"
+    println(calcularNumeroLetras(cadee))
 }
 
-fun String.unaccent(): String {
-    return "\\p{InCombiningDiacriticalMarks}+".toRegex().replace(Normalizer.normalize(this, Normalizer.Form.NFD), "")
-}
-
-fun calcularNumeroLetras(cad: String):MutableMap<String, Int> {
-    var map :MutableMap<String, Int> = mutableMapOf()
-    var cont = 0
-    var tamanio = cad.length
-    
-    while(cont < tamanio) {
-        var lett = cad[cont].toString()
-        if(map.containsKey(lett)) {
-            map[lett] = map.getValue(lett) + 1
-        } else {
-            map.put(lett, 1)
-        }
-
-        cont++
-    }
-        
-    return map
+fun calcularNumeroLetras(cad: String):Int {
+    return cad.replace(("[^A-Za-z]").toRegex(), "").length
 }
