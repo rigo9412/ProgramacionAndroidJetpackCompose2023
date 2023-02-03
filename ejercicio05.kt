15b7e3100a37512157552092be01835dcf6a4dd9
JaimeVJ-TEC <l19100265@nlaredo.tecnm.mx>
2023-02-03 09:18:18 -0600

    cambios a ejercicio 5

--- ejercicio05.kt
+++ ejercicio05.kt
@@ -1,17 +1,19 @@
 //Elaborar una función que reciba un texto y cuente cuantas veces aparece una letra en el texto (solo letras)
 //ejemplo a=5, o=40.
 import java.text.Normalizer
+import java.util.TreeMap
 
 fun main(){
     println(ContarLetras("Lorém ipsum dolor sit amet augéndas civitas efficiántur firmitatem"))
 }
 
-fun ContarLetras(texto : String) : Map<Char,Int>{
+fun ContarLetras(texto : String) : String{
     var cadena = texto
     cadena = Normalizer.normalize(cadena, Normalizer.Form.NFD)
     cadena = cadena.lowercase()
 
-    var mapa = mutableMapOf<Char,Int>()
+    //var mapa = mutableMapOf<Char,Int>()
+    var mapa = TreeMap<Char, Int>()
     var i = 0
     while(i < cadena.length){
         if(cadena[i].isLetter()){
@@ -21,5 +23,6 @@ fun ContarLetras(texto : String) : Map<Char,Int>{
         }
         i += 1
     }
-    return mapa;
+    var mensaje = mapa.toString()
+    return mensaje.substring(1,mensaje.length-1)
 }
