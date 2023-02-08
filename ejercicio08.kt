d6778f709356bf4200d99ae92cc93ca119039c4a
ReneSalinas <renesalinas0525@gmail.com>
2023-02-08 17:21:39 -0600

    ejercicio08

--- ejercicio08.kt
+++ ejercicio08.kt
@@ -1,12 +1,25 @@
-//PROFE
-fun combineArrays(vararg data: Array<Any>): Array<Any>{
-    val resulArray = mutableListOf<Any>()
-    val combineArrays = mutableListOf<Any>()
-    for (item in data) combineArrays.addAll(item)
+//Cree una función que tome dos o más arrays y devuelva una array de sus
+// diferencias. La array que se devuelve debe contener solo valores únicos (no
+// duplicados).
 
-    combineArrays.groupBy { it.toString()}.forEach {it.Map.Entry<Stirng.List<Any>>
-        if (it.value.count() == 1) resultArray.add(it.key)
-    }
-
-    return resulArray.toTypedArray()
+fun main(){  
+    println(combinados(arrayOf(3), arrayOf(1,2,3,4,0,0,"a","null"), arrayOf("2",1,0,"A",9,"NULL")).joinToString())
+    
 }
+
+
+fun combinados(vararg array: Array<Any>):Array<Any> 
+{
+    var lista = mutableListOf<Any>()
+    var difernetes = mutableListOf<Any>()
+
+    for (i in array) difernetes.addAll(i)
+
+    difernetes.groupBy{it.toString()}.forEach{ 
+            if(it.value.count()==1) lista.add(it.key) 
+    } 
+    
+    
+    return lista.toTypedArray()
+}
+
