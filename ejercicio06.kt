d58182e7cdaf0a5e93b23ec0e707882cd17c6d63
Alan Peña Ortiz <alancin7@live.com.mx>
2023-02-08 17:59:13 -0600

    code: segunda version para determinar

--- ejercicio06.kt
+++ ejercicio06.kt
@@ -4,6 +4,17 @@ fun determinar(n1: Int, n2: Int, n3: Int): Boolean {
     return recorrer.any { valores.any { a -> it.contains(a) } }
 }
 
+//Supongo sin tanto map o any  es mas rapido
+fun detemrminar2(n1: Int, n2: Int, n3: Int): Boolean {
+    val nn1 = (n1%10).toString()
+    val nn2 = (n2%10).toString()
+    val nn3 = (n3%10).toString()
+    val s1 = n1.toString().contains(nn2) || n1.toString().contains(nn3)
+    val s2 = n2.toString().contains(nn1) || n2.toString().contains(nn3)
+    val s3 = n3.toString().contains(nn1) || n3.toString().contains(nn2)
+    return s1 || s2 || s3
+}
+
 fun main(){
-    println(determinar(123, 456, 389))
+    println(detemrminar2(1, 4, 4))
 }
