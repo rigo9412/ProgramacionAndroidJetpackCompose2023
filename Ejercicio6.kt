7630d2d786255831b1b9cb254832ca34d5140dba
ezequielcantu21 <70524684+Ezequiel-Cantu@users.noreply.github.com>
2023-02-08 09:08:22 -0600

    Ejercicio6

--- Ejercicio6.kt
+++ Ejercicio6.kt
@@ -2,11 +2,11 @@ fun main() {
    println(ultimoDigito(278,617,288))
 }
 
-fun ultimoDigito(numero1:Int,numero2:Int,numero3:Int): String {
+fun ultimoDigito(numero1:Int,numero2:Int,numero3:Int): Boolean {
     val result = setOf(numero1%10, numero2%10, numero3%10).size == 3
     if (result){
-        return "No se repite el último digito de ningun número"
+        return false
     }else{
-        return "El último digito de algun número se repite"
+        return true
     }
 }
