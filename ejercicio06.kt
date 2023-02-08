e6fb36c1f1ef3eec536ac54ffd872d76071d8a3d
AndreaMartinezLimon-Tec <joandymart_lim@hotmail.com>
2023-02-08 17:07:37 -0600

    Arreglado ejercicio 6

--- ejercicio06.kt
+++ ejercicio06.kt
@@ -3,10 +3,20 @@ dígito del número se repite entre alguno de los 3 números.
  */
 
 fun main() {
-   print(determinarNumero(15,17,22))
+   print(determinarNumero2(64,71,5962426))
 }
 
-fun determinarNumero(n1:Int,n2:Int,n3:Int): Boolean {
-    val result = setOf(n1%10, n2%10, n3%10).size == 3
+fun determinarNumero1(n1:Int,n2:Int,n3:Int): Boolean {
+    val result = setOf(n1%10, n2%10, n3%10).size != 3
     return result
+}
+
+fun determinarNumero2(n1:Int,n2:Int,n3:Int): Boolean {
+    val num1 = (n1%10).toString()
+    val num2 = (n2%10).toString()
+    val num3 = (n3%10).toString()
+    val isExist1 = num1 in n2.toString() || num1 in n3.toString() || num1 in n1.toString().dropLast(n1.toString().count())
+    val isExist2 = num2 in n1.toString() || num2 in n3.toString() || num2 in n2.toString().dropLast(n2.toString().count())
+    val isExist3 = num3 in n2.toString() || num3 in n2.toString() || num3 in n3.toString().dropLast(n3.toString().count())
+    return (isExist1 ||isExist2 ||isExist3)
 }
--- ejercicio08.kt
+++ ejercicio08.kt
@@ -4,37 +4,18 @@ duplicados).
  */
 
 fun main() {
-   val items = listOf(1,2,3,4)
-   val items1 = listOf(1,1,3,7)
-   diferencias(items,items1)
+    val result = diferencias(
+    arrayOf(3),
+    arrayOf(1,2,3,4,0,0,"a","null"),
+    arrayOf("2",1,0,"A",9,"NULL"))
+   print(diferencias(arrayOf(result)))
 }
-fun <T> diferencias(vararg array: List<T>) {
-    var list = ArrayList<T>()
-    var index = 0
-    var u = 0
-    while(index < array.size){
-        while(u < array.get(index).size){
-            list.add(array.get(index).get(u))
-        u++
+fun diferencias(vararg array: Array<Any>) : String {
+    var list = ArrayList<Any>()
+    var list1 = ArrayList<Any>()
+    for(item in array) list.addAll(item)
+    list.groupBy{it.toString()}.forEach{
+        if(it.value.count() == 1) list1.add(it.key)
     }
-        index++
-        u=0
-    }
-    val diferencia = list.distinct()
-    print (diferencia)
-}
-
-fun main() {
-   print(diferencias(arrayOf(3),arrayOf(1,2,3,4,0,0,"a","null"), arrayOf("2",1,0,"A",9,"NULL")).joinToString())
-   
-}
-fun diferencias(vararg array: Array<Any>) : Array<Any> {
-    val resultArray = mutableListOf<Any>()
-    val contains = mutableListOf<Any>()
-    for(item in array) contains.addAll(item)
-    
-    contains.groupBy{it.toString()}.forEach{
-        if(it.value.count() == 1) resultArray.add(it.key)
-    }
-    return resultArray.toTypedArray()
+    return (list1.joinToString())
 }
