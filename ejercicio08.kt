98df73d59f8dbfe8512ffb5d108d702d973eaa89
Alan Peña Ortiz <alancin7@live.com.mx>
2023-02-07 22:31:15 -0600

    code: ahora funciona con cualquier tipo

--- ejercicio08.kt
+++ ejercicio08.kt
@@ -1,9 +1,10 @@
-fun diferencias(vararg arrays: Array<Int>): Array<Int> {
-    val merged = arrays.reduce { acc, array -> acc + array }
-    return merged.distinct().toTypedArray()
+fun diferencias(vararg arrays: Array<Any>): String {
+    return arrays.map { it.map { any -> any.toString()  }  }.reduce{ acc, arr -> acc + arr}.distinct().joinToString(",")
 }
 
-fun main(args: Array<String>) {
-    println(diferencias(arrayOf(1, 2, 3), arrayOf(2, 5, 2, 1, 4, 7, 1)).contentToString())
-    println(diferencias(arrayOf(1, 2, 3), arrayOf(2, 5, 2, 1, 4, 9), arrayOf(2, 1, 2)).contentToString())
+fun main() {
+    val x = diferencias(arrayOf(3),
+        arrayOf(1,2,3,4,0,0,"a","null"),
+        arrayOf("2",1,0,"A",9,"NULL"))
+    println(x)
 }
