41da713c176d058521f38e0ebc6fb8befb658820
cdlr247 <cdlr01@hotmail.com>
2023-02-09 09:11:39 -0600

    ejercicio 8 actualizado

--- Unidad 1/ejercicio08.kt
+++ Unidad 1/ejercicio08.kt
@@ -1,7 +1,7 @@
 fun main()
 {
-    var array1 : Array<Any> = arrayOf(1,2,3,4,0,0,"a","null").toMutableList().distinct().toTypedArray()
-    var array2 : Array<Any> = arrayOf("2",1,0,"A",9,"NULL").toMutableList().distinct().toTypedArray()
+    var array1 : Array<Any> = arrayOf(1,2,3,4,0,0,"a","null")//.toMutableList().distinct().toTypedArray()
+    var array2 : Array<Any> = arrayOf("2",1,0,"A",9,"NULL")//.toMutableList().distinct().toTypedArray()
     // 4,a,null,A,9,NULL resultado
     var array = sacarDiferencia(array1, array2, arrayOf(3))
     for(c in array)
@@ -15,12 +15,11 @@ fun sacarDiferencia(vararg array: Array<Any>):Array<Any>
     var lista : MutableList<String> = mutableListOf()
     for (c in array)
     {
-        for(s in c)
+        for(s in c.toMutableList().distinct())
         {
             var existe : String? = lista.firstOrNull { it == s.toString() }
             if (existe!=null)
             {
-                //var index = lista.indexOf(existe)
                 lista.remove(existe)
             }
             else{
@@ -29,7 +28,6 @@ fun sacarDiferencia(vararg array: Array<Any>):Array<Any>
             
         }
     }
-    //lista = lista.toSet().toMutableList()
     return lista.toTypedArray()
 }
 
