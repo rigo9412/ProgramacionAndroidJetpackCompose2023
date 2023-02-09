5ed7235db553d15b5493382664880b4cb8692e6b
JaimeVJ-TEC <L19100265@nlaredo.tecnm.mx>
2023-02-08 19:16:13 -0600

    cambios al ejercicio 6 y 8

--- ejercicio06.kt
+++ ejercicio06.kt
@@ -4,9 +4,28 @@
 import java.util.ArrayDeque
 
 fun main(){
-    println(SeRepite(10,2,32))
+    println(SeRepite(1,4,4))
+    println(isInLastDigit(1,4,4))
 }
 
+//Version 2
+fun isInLastDigit(x : Int, y : Int, z : Int) : Boolean{
+    val nums = intArrayOf(x,y,z)
+    val ultimos: IntArray = intArrayOf((x % 10), (y % 10), (z % 10))
+    var stack = ArrayDeque<Int>()
+    for (i in nums) {
+        var temp = i
+        temp = temp / 10
+        while(temp != 0)
+        {
+            stack.push(temp % 10)
+            temp = temp / 10
+        }
+    }
+    return (ultimos.intersect(stack).count() != 0) || (setOf(x,y,z).size != 3)
+}
+
+//Version 1
 fun SeRepite(x : Int, y : Int, z : Int) : Boolean{
     val nums = intArrayOf(x,y,z)
     val ultimos: IntArray = intArrayOf((x % 10), (y % 10), (z % 10))
