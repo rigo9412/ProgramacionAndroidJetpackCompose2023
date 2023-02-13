e5c0a5ee0a2b91927127e20ef9f8f6f572c7250d
mikeangelo1212 <70547488+mikeangelo1212@users.noreply.github.com>
2023-02-13 09:20:02 -0600

    Optimizacion y correccion del ejercicio 9

--- Unidad 1/ejercicio09.kt
+++ Unidad 1/ejercicio09.kt
@@ -9,7 +9,7 @@
 //      detener el proceso e indicar en que paso lo encontró
 
 fun main(){
-    multiODiv(2,2)
+    multiODiv(593,2)
 }
 
 
@@ -18,39 +18,29 @@ fun multiODiv(valor:Int,tope:Int):String
     var i=0
     var pares=0
     var inpares=0
+    var nuevoValor=valor
     //preguntamos si el residuo de una division en dos es 0, si es asi es par
     //sino es impar
-    if(valor%2==0){
-        pares++
-        var valor=valor/2
-        println("$valor\nPASO=$i,PARES=$pares,INPARES=$inpares\nNO SE ENCONTRO EN EL NUMERO $tope")
-        return ("$valor\nPASO=$i,PARES=$pares,INPARES=$inpares\nNO SE ENCONTRO EN EL NUMERO $tope")
-    }
-    else {
-//        println("Valor ($valor) es impar, multiplicado por tres es: ${valor * 3}")
-        var nuevoValor=(valor*3)+1
-        var seEncontroNumero="$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nSE ENCONTRO EN EL NUMERO $tope EN EL PASO:$i"
-        while (nuevoValor!=1){
+    while(nuevoValor!=1) {
 
-            //modificacion b, preguntamos si el otro numero se encuentra en los pasos
-            if(nuevoValor==tope){
-                println("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nSE ENCONTRO EN EL NUMERO $tope EN EL PASO:$i")
-                return ("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nSE ENCONTRO EN EL NUMERO $tope EN EL PASO:$i")
-            }
-
-            //si es par dividimos, sino multiplicamos y le sumamos uno
-            if(nuevoValor%2==0){
-                nuevoValor=nuevoValor/2
-                pares++
-            }
-            else {
-                nuevoValor = (nuevoValor * 3) + 1;inpares++
-            }
-            i++
+        //modificacion b, preguntamos si el otro numero se encuentra en los pasos
+        if (nuevoValor == tope) {
+            println("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nSE ENCONTRO EN EL NUMERO $tope EN EL PASO:$i")
+            return ("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nSE ENCONTRO EN EL NUMERO $tope EN EL PASO:$i")
         }
+
+        //si es par dividimos, sino multiplicamos y le sumamos uno
+        if (nuevoValor % 2 == 0) {
+            nuevoValor = nuevoValor / 2
+            pares++
+        } else {
+            nuevoValor = (nuevoValor * 3) + 1;inpares++
+        }
+        i++
+    }
         //imprimimos lo ultimo sin haber encontrado el numero
         println("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nNO SE ENCONTRO EN EL NUMERO $tope")
         return ("$nuevoValor\nPASO=$i,PARES=$pares,INPARES=$inpares\nNO SE ENCONTRO EN EL NUMERO $tope")
 
-    }
+
 }
