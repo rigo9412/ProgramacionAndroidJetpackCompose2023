e0511b45ef813f4970a0bb563797848010a1cfb4
¨RevisOTO¨ <rafamosqueda.barragan12@gmail.com>
2023-02-13 09:36:12 -0600

    Tarea 9

--- ejercicio9.kt
+++ ejercicio9.kt
@@ -1,23 +1,21 @@
 fun main() {
-    ruleN3(10_017_019_990_047_100,11272)
+    println(ruleN3(10_017_019_990_047_100,55))
 }
  
-fun ruleN3(value: Long, value1: Long) {
+fun ruleN3(value: Long, value1: Long): String {
     var pasos :Int = 1;
     var pares :Int = 0;
     var impares :Int = 1;
     var num = value
     while(num != 1L){
-        println(num)  
+        if(num == value1) {return "SE ENCONTRO EN EL NUMERO " + num + " EN EL PASO: " + (pasos);}
         if(num % 2L == 0L) {num = num/2L; pares += 1;}
         else {
             num = (num * 3L) + 1L; 
             impares += 1
         }
-        if(num == value1) {println("Se encontro el numero " + value1 + " en el paso " + (pasos + 1)); return;}
         pasos += 1
     }
-    println("Numeros pares: " + pares)
-    println("Numeros impares: " + impares)
-    println("Numeros pasos: " + pasos)
+    return "PASO=" + pasos + " PARES=" + pares + " IMPARES=" + impares 
+    
 }
