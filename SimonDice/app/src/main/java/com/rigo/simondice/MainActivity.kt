6eee4acbcbcb19232d3b79410dd360c7751a0add
rigo9412 <rigo.ramos.9412@hotmail.com>
2023-02-22 23:47:02 -0600

    interaccion del jugador

--- SimonDice/app/src/main/java/com/rigo/simondice/MainActivity.kt
+++ SimonDice/app/src/main/java/com/rigo/simondice/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.rigo.simondice
 
-import android.app.GameState
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -15,26 +17,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Device
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.rigo.simondice.domain.models.Actions
+import com.rigo.simondice.domain.models.Action
 import com.rigo.simondice.domain.models.Game
 import com.rigo.simondice.ui.theme.*
-import kotlinx.coroutines.Delay
 import kotlinx.coroutines.delay
-import java.time.Duration
+
 
 class MainActivity : ComponentActivity() {
-    val game = Game()
+   private val game = Game()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var currentActionIndexState by remember { mutableStateOf(-1) }
+            val blueAudio: MediaPlayer by remember { mutableStateOf(MediaPlayer.create(this, R.raw.blue)) }
+            val redAudio: MediaPlayer by remember { mutableStateOf(MediaPlayer.create(this, R.raw.red)) }
+            val yellowAudio: MediaPlayer by remember { mutableStateOf(MediaPlayer.create(this, R.raw.yellow)) }
+            val greenAudio: MediaPlayer by remember { mutableStateOf(MediaPlayer.create(this, R.raw.green)) }
+
+            var currentActionIndexState by remember { mutableStateOf(game.currentActionIndex) }
+            var currentActionPlayerIndexState by remember { mutableStateOf(game.currentActionPlayerIndex) }
+            var currentActionPlayer by remember { mutableStateOf(Action.NO_ACTION) }
             var currentActionOn by remember { mutableStateOf(false) }
             var startGameState by remember { mutableStateOf(game.started) }
-
+            var startPlayState by remember { mutableStateOf(game.endSpeak) }
 
             SimonDiceTheme {
                 // A surface container using the 'background' color from the theme
@@ -43,31 +51,96 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     LaunchedEffect(currentActionIndexState) {
-                        if(startGameState) {
-                            delay(200)
+                        if (startGameState && !currentActionOn) {
+                            when (game.getCurrentAction()) {
+                                Action.PRESS_GREEN_BUTTON -> playAudio(greenAudio)
+                                Action.PRESS_RED_BUTTON -> playAudio(redAudio)
+                                Action.PRESS_YELLOW_BUTTON -> playAudio(yellowAudio)
+                                Action.PRESS_BLUE_BUTTON ->  playAudio(blueAudio)
+                                else -> {
+                                    //NO SUENA
+                                }
+                            }
+
+                            currentActionOn = true
+                            delay(1000)
                             currentActionOn = false
-                            delay(200)
                             game.moveToNextAction()
                             currentActionIndexState = game.currentActionIndex
-                            currentActionOn = true
+                            //currentActionOn = false
+                        }
+                        startPlayState = game.endSpeak
+                    }
 
-                            if(currentActionIndexState == game.lastActionIndex){
-                                game.end("dummy")
-                                startGameState = false
-                                currentActionIndexState = -1
+                    LaunchedEffect(currentActionPlayer) {
+                        if (startGameState && game.endSpeak && currentActionPlayer != Action.NO_ACTION && !currentActionOn) {
+                            when (currentActionPlayer) {
+                                Action.PRESS_GREEN_BUTTON ->  playAudio(greenAudio)
+                                Action.PRESS_RED_BUTTON -> playAudio(redAudio)
+                                Action.PRESS_YELLOW_BUTTON -> playAudio(yellowAudio)
+                                Action.PRESS_BLUE_BUTTON ->  playAudio(blueAudio)
 
+                                else -> {
+                                    //NO SUENA
+                                }
                             }
+                            currentActionOn = true
+                            delay(300)
+                            currentActionOn = false
+                            currentActionPlayer = Action.NO_ACTION
+//                            currentActionOn = false
+//
+//                            currentActionPlayerIndexState = game.currentActionPlayerIndex
+//                            if(!game.validateAction(currentActionPlayer)){
+//                                println("END GAME")
+//                                game.end("dummy")
+//                                startGameState = game.started
+//                                currentActionIndexState = game.currentActionIndex
+//                            }
+
+                           // currentActionPlayerIndexState = game.currentActionPlayerIndex
                         }
                     }
 
                     Column() {
-                        SimonGame(game.endSpeak, game.getCurrentAction(),currentActionOn)
-                        StartButton(startGameState,onStart = {
+                        SimonGame(
+                            startPlayState,
+                            game.getCurrentAction(),
+                            actionPlayer = currentActionPlayer,
+                            currentActionOn) {
+
+                                currentActionPlayer = it
+
+
+                        }
+
+                        StartButton(startGameState, onStart = {
                             game.start()
                             currentActionIndexState = game.currentActionIndex
                             startGameState = game.started
 
                         })
+
+                            Text(text = "STATUS GAME $startGameState")
+                            Text(text = "STATUS PLAY GAME $startPlayState")
+                            Text(text = "SPEAK ${game.endSpeak}")
+                            Text(text = "SCORE ${game.score}")
+                            Text(text = "LEVEL ${game.level}")
+                            Text(text = "ACTION SIMON ${game.getCurrentAction().toString()}")
+                            Text(text = "ACTION ${currentActionPlayer.toString()}")
+                            Text(text = "ACTION ON/OFF ${currentActionOn.toString()}")
+                            Text(text = "INDEX PLAYER ACTION ON/OFF ${currentActionPlayerIndexState.toString()}")
+                            Text(text = "INDEX SIMON ON/OFF ${currentActionIndexState.toString()}")
+                            Button(onClick = {
+                                game.end("dummy")
+                                startGameState = game.started
+                                currentActionIndexState = game.currentActionIndex
+
+                            }) {
+                                Text(text = "RESET")
+                            }
+
+
                     }
                 }
             }
@@ -76,23 +149,36 @@ class MainActivity : ComponentActivity() {
 }
 
 
+private fun playAudio(mp: MediaPlayer) {
+    println("AUDIO ${mp.isPlaying}")
+        if(!mp.isPlaying) {
+            mp.start()
+        }else{
+            mp.pause();
+            mp.seekTo(0);
+            mp.start()
+        }
 
 
-
+}
 @Composable
-fun SimonGame(simonSpeak: Boolean,action: Actions?,offCurrent: Boolean){
+fun SimonGame(endSpeak: Boolean, action: Action,actionPlayer:Action, offCurrent: Boolean, onClick: (action: Action) -> Unit) {
 
     Column(
         modifier = Modifier.background(Color.Black),
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Pad(action,offCurrent)
+        Pad(if(endSpeak) actionPlayer else action, offCurrent,enablePlay = endSpeak,onClick)
+
     }
 }
 
 @Composable
-fun Pad(action: Actions?,offCurrent: Boolean) {
+fun Pad(action: Action?, onCurrent: Boolean, enablePlay: Boolean,onClick: (action: Action) -> Unit) {
+
+
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,27 +187,79 @@ fun Pad(action: Actions?,offCurrent: Boolean) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            ButtonAction(on = action == Actions.PRESS_GREEN_BUTTON && !offCurrent , colorOn = GreenOn, colorOff = GreenOff, colorText = "Verde")
-            ButtonAction(on = action == Actions.PRESS_RED_BUTTON && !offCurrent, colorOn = RedOn, colorOff = RedOff, colorText = "Rojo")
+            ButtonAction(
+                on = action == Action.PRESS_GREEN_BUTTON && onCurrent,
+                colorOn = GreenOn,
+                colorOff = GreenOff,
+                colorText = "Verde",
+                enablePlay = enablePlay,
+                action = Action.PRESS_GREEN_BUTTON,
+                onClick =  onClick
+            )
+            ButtonAction(
+                on = action == Action.PRESS_RED_BUTTON && onCurrent,
+                colorOn = RedOn,
+                colorOff = RedOff,
+                colorText = "Rojo",
+                enablePlay = enablePlay,
+                action = Action.PRESS_RED_BUTTON,
+                onClick = onClick
+
+            )
         }
 
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            ButtonAction(on = action == Actions.PRESS_BLUE_BUTTON && !offCurrent, colorOn = BlueOn, colorOff = BlueOff, colorText = "Azul")
-            ButtonAction(on = action == Actions.PRESS_YELLOW_BUTTON && !offCurrent, colorOn = YellowOn, colorOff = YellowOff, colorText = "Amarillo")
+            ButtonAction(
+                on = action == Action.PRESS_BLUE_BUTTON && onCurrent,
+                colorOn = BlueOn,
+                colorOff = BlueOff,
+                colorText = "Azul",
+                enablePlay = enablePlay,
+                action = Action.PRESS_BLUE_BUTTON,
+                onClick = onClick
+            )
+            ButtonAction(
+                on = action == Action.PRESS_YELLOW_BUTTON && onCurrent,
+                colorOn = YellowOn,
+                colorOff = YellowOff,
+                colorText = "Amarillo",
+                enablePlay = enablePlay,
+                action = Action.PRESS_YELLOW_BUTTON,
+                onClick = onClick
+            )
         }
     }
 }
 
 @Composable
-fun ButtonAction(on: Boolean,colorOn: Color, colorOff: Color,colorText: String){
-    Box( modifier = Modifier
-        .width(100.dp)
-        .height(100.dp)
-        .padding(4.dp)
-        .background(if (on) colorOn else colorOff),
+fun ButtonAction(
+    on: Boolean,
+    colorOn: Color,
+    colorOff: Color,
+    colorText: String,
+    enablePlay: Boolean,
+    action: Action,
+    onClick: (Action) -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    Box(
+
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+            .padding(4.dp)
+            .background(if (on) colorOn else colorOff)
+            .clickable(
+            interactionSource = interactionSource,
+            indication = null
+        ) {
+          if(!on && enablePlay){
+              onClick(action)
+          }
+        },
         contentAlignment = Alignment.Center
     ) {
         Text(text = colorText, fontWeight = FontWeight(900))
@@ -129,7 +267,7 @@ fun ButtonAction(on: Boolean,colorOn: Color, colorOff: Color,colorText: String){
 }
 
 @Composable
-fun StartButton(startGameState: Boolean, onStart: () -> Unit){
+fun StartButton(startGameState: Boolean, onStart: () -> Unit) {
     Button(
         enabled = !startGameState,
         onClick = { onStart() }) {
@@ -142,8 +280,8 @@ fun StartButton(startGameState: Boolean, onStart: () -> Unit){
 @Composable
 fun DefaultPreview() {
     SimonDiceTheme {
-        val dummyAction = Actions.PRESS_RED_BUTTON
-        SimonGame(false,dummyAction,false)
-        
+        val dummyAction = Action.PRESS_RED_BUTTON
+        SimonGame(false, dummyAction,dummyAction, false, onClick = {})
+
     }
 }
--- SimonDice/app/src/main/java/com/rigo/simondice/domain/models/Game.kt
+++ SimonDice/app/src/main/java/com/rigo/simondice/domain/models/Game.kt
@@ -3,32 +3,34 @@ package com.rigo.simondice.domain.models
 import kotlin.random.Random
 
 class Game() {
-    private val INCREMENT_BY_LEVEL = 4
+    private val INCREMENT_BY_LEVEL = 2
     private val INCREMENT_BY_SCORE = 10
-
     private var _score = 0
-
     private var _level = 1
-    private var _currentActionIndex = 0
-    private var _listActions = mutableListOf<Actions>()
+    private var _currentActionIndex = -1
+    private var _currentActionPlayerIndex = 0
+    private var _listActions = mutableListOf<Action>()
 
     val score get() = this._score
     val level get() = this._level
 
     val started get() = _listActions.isNotEmpty()
-    val endSpeak get() = _listActions.isNotEmpty() && _listActions.lastIndex == _currentActionIndex
+    val endSpeak get() = _listActions.isNotEmpty() && _listActions.lastIndex < _currentActionIndex
     val currentActionIndex get() = _currentActionIndex
+    val currentActionPlayerIndex get() = _currentActionPlayerIndex
     val lastActionIndex get() = _listActions.lastIndex
 
 
-    fun getCurrentAction(): Actions?{
-        return if(_listActions.isNotEmpty())  _listActions[_currentActionIndex] else null
+    fun getCurrentAction(): Action{
+        return if(_listActions.isNotEmpty() && _currentActionIndex >= 0 && _currentActionIndex <= _listActions.lastIndex)  _listActions[_currentActionIndex] else Action.NO_ACTION
     }
 
-    fun moveToNextAction(): Actions?{
-        if(_listActions.isEmpty()) return null
-        if(_currentActionIndex == _listActions.lastIndex) return null
-
+    fun moveToNextAction(): Action{
+        if(_listActions.isEmpty() || _currentActionIndex > _listActions.lastIndex) return Action.NO_ACTION
+        if(_currentActionIndex == _listActions.lastIndex){
+            _currentActionIndex++
+            return Action.NO_ACTION
+        }
         _currentActionIndex++
         return _listActions[_currentActionIndex]
 
@@ -37,6 +39,8 @@ class Game() {
     fun start() : Game{
         _score  = 0
         _level = 1
+        _currentActionPlayerIndex = 0
+        _currentActionIndex = 0
         generateActions()
 
         return this
@@ -46,21 +50,23 @@ class Game() {
         val player = Player(namePlayer,_score,_level)
         _score = 0
         _level = 1
-        _currentActionIndex = 0
+        _currentActionIndex = -1
+        _currentActionPlayerIndex = -1
         _listActions.clear()
         return player
     }
 
-    fun validateAction(index: Int, action: Actions): Boolean{
-        if(_listActions[index] == action){
-            _currentActionIndex++
+    fun validateAction(action: Action): Boolean{
+        if(_listActions[_currentActionPlayerIndex] != action) return  false
 
-            if(_listActions.lastIndex == index)
-                newLevel()
+        _currentActionPlayerIndex++
 
-        }
+        if(_listActions.lastIndex == _currentActionPlayerIndex)
+            newLevel()
 
-        return false
+
+
+        return true
     }
 
 
@@ -69,15 +75,13 @@ class Game() {
         val maxActions = _level * INCREMENT_BY_LEVEL
         _listActions.clear()
         for (i in 0.. maxActions){
-            _listActions.add(Actions.values()[Random.nextInt(0, Actions.values().lastIndex)])
+            _listActions.add(Action.values()[Random.nextInt(1, Action.values().count())])
         }
-
-
     }
 
     private fun newLevel(){
-        _score += (INCREMENT_BY_SCORE * INCREMENT_BY_LEVEL)
         _level++
+        _score += (INCREMENT_BY_SCORE * _level)
         generateActions()
     }
 
