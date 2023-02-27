82f018b74a10a911b351e1c9f62c4c13d742be96
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-02-27 07:35:14 -0600

    Agrego Ejercicio de las cabezasminecraft

---
+++ Parcial1/CabezasMinecraft/app/src/main/java/com/example/cabezasminecraft/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.cabezasminecraft.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val nosePinkDark = Color(0xFF815F5A)
+val nosePink = Color(0xFFFABFB7)
+val basePink = Color(0xFFF9A195)
+
+val baseSkullHead = Color(0xFFCFCFCF)
+
+val baseGreen = Color(0xFF4CAF50)
+val Green = Color(0xFF0E3110)
+val eyeDarker = Color(0xFF000000)
---
+++ Parcial1/CabezasMinecraft/app/src/main/java/com/example/cabezasminecraft/ui/theme/Pig.kt
@@ -0,0 +1,137 @@
+package com.example.cabezasminecraft.ui.theme
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.cabezasminecraft.R
+
+import com.example.cabezasminecraft.ui.theme.CabezasMinecraftTheme
+
+
+class Pig : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CabezasMinecraftTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Eye(left: Boolean)
+{
+    Row(){
+        Box(modifier = Modifier
+            .background(if (left) Color.Black else Color.White)
+            .width(50.dp)
+            .height(50.dp))
+        Box(modifier = Modifier
+            .background(if (left) Color.White else Color.Black)
+            .width(50.dp)
+            .height(50.dp))
+    }
+}
+
+@Composable
+fun Eyes(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Eye( left=true)
+        Eye( left=false)
+    }
+}
+
+@Composable
+fun Nostrils(){
+    Box(
+        modifier = Modifier
+            .background(nosePinkDark)
+            .width(40.dp)
+            .height(40.dp)
+    )
+}
+
+@Composable
+fun Nose(modifier: Modifier){
+    Row(
+        modifier = modifier
+            .width(180.dp)
+            .height(120.dp)
+            .background(nosePink),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Nostrils()
+        Nostrils()
+    }
+}
+
+@Composable
+fun Face(){
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (eyes,nose) = createRefs()
+        Eyes(modifier = Modifier.constrainAs(eyes){
+            bottom.linkTo(nose.top)
+        })
+        Nose(modifier = Modifier
+            .constrainAs(nose) {
+                top.linkTo(parent.top,70.dp)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+    }
+}
+
+@Composable
+fun HeadPig(/*onClick: () -> Unit,*/ context: Context?){
+    Column(
+        modifier = Modifier.fillMaxSize()/*.clickable { onClick() }*/,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier
+                .background(basePink)
+                .width(600.dp)
+                .height(400.dp)
+                .clickable {
+                    MediaPlayer
+                        .create(context, R.raw.pig)
+                        .start()
+                }
+        ) {
+            Face()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewPig() {
+    CabezasMinecraftTheme {
+        //HeadPig()
+    }
+}
