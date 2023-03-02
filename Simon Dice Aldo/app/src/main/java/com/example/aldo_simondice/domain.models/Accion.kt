43ef791e2f2abcb310575fa72e2c83dd5bf0d2a0
AlMy 3 <70553666+Aldo-Ezequiel-Rodriguez-Mendez@users.noreply.github.com>
2023-03-01 21:59:58 -0600

    SimonDice

---
+++ Simon Dice Aldo/app/src/main/java/com/example/aldo_simondice/domain.models/Accion.kt
@@ -0,0 +1,10 @@
+package com.example.aldo_simondice.domain.models
+
+enum class Accion(val value: Int){
+    NO_ACCION(-1),
+    PRESIONAR_BOTON_Morado(0),
+    PRESIONAR_BOTON_Naranja(1),
+    PRESIONAR_BOTON_Rosa(2),
+    PRESIONAR_BOTON_Azulado(3)
+}
+
---
+++ Simon Dice Aldo/app/src/main/java/com/example/aldo_simondice/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.aldo_simondice.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val MoradoEncendido = Color(0xFF9C27B0)
+val MoradoApagado= Color(0xFF6200EE)
+
+val NaranjaEncendido= Color(0xFFFF5722)
+val NaranjaApagado= Color(0xFFA82A03)
+
+val AzuladoEncendido = Color(0xFF009688)
+val AzuladoApagado= Color(0xFF016B61)
+
+val RosaEncendido = Color(0xFFF340B1)
+val RosaApagado = Color(0xFFC509BC)
