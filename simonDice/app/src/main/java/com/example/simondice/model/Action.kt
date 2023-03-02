584c8f266acd35e7e259655debc5eee9125027c2
Otto10 <ottomaldonado01@gmail.com>
2023-03-01 19:19:39 -0600

    ejercicios 14-15-16

--- simonDice/app/src/main/java/com/example/simondice/model/Action.kt
+++
@@ -1,9 +0,0 @@
-package com.example.simondice.model;
-
-enum class Action(val value: Int) {
-    NO_ACTION(-1) ,
-    PRESS_GREEN_BUTTON(0) ,
-    PRESS_RED_BUTTON(1),
-    PRESS_BLUE_BUTTON(2),
-    PRESS_YELLOW_BUTTON(3)
-}
