83e93473a1a65e843b92d117ededf061de2c025c
AlejandroDiaz <alexanfmdm@gmail.com>
2023-03-01 21:43:44 -0600

    SimonDice

---
+++ unidad1/Simonv2/app/src/main/java/com/simonv2/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.simonv2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val LG = Color(0xFF646969)
+
+val RedOn = Color(0xFFf44336)
+val RedOff = Color(0xFFB71C1C)
+
+val YellowOn = Color(0xFFffee58)
+val YellowOff = Color(0xFF827717)
+
+val BlueOn = Color(0xFF2196F3)
+val BlueOff = Color(0xFF0D47A1)
+
+val GreenOn = Color(0xFF66bb6a)
+val GreenOff = Color(0xFF1b5e20)
