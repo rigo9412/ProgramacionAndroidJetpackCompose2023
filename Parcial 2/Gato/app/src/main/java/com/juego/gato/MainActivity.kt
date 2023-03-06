f91d4dc6d1a2571befe8a28b21725cd492321ed5
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2023-03-06 09:39:58 -0600

    Reorganizando rama

---
+++ Parcial 2/Gato/app/src/main/java/com/juego/gato/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.juego.gato
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+                val viewModel = viewModel<GameViewModel>()
+                GameScreen(
+                    viewModel = viewModel
+                )
+
+        }
+    }
+}
