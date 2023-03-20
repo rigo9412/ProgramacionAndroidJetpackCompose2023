c8823a8c2e2c3b880bbd4a15fa5da1bd26122e3a
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-03-20 02:14:13 -0600

    .

---
+++ Parcial2/CURP/app/src/main/java/com/game/curp/forms/components/CustomInput.kt
@@ -0,0 +1,39 @@
+package com.game.curp.forms.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+
+@Composable
+fun CustomInput(
+    label: String,
+    value: String,
+    onChangeValue: (String) -> Unit,
+    modifier: Modifier,
+    focusManager: FocusManager
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onChangeValue,
+        modifier = modifier,
+        label = { Text(label) },
+        isError = false,
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(
+            onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }
+        )
+    )
+}
