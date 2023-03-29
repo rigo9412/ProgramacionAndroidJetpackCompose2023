dd18a5f8ce7faa47a292c844aa5742634d19ebd8
Alan Peña Ortiz <alancin7@live.com.mx>
2023-03-29 09:55:48 -0500

    Bottom navigation bar + cositas

--- app/src/main/java/com/lanazirot/pokedex/MainActivity.kt
+++ app/src/main/java/com/lanazirot/pokedex/MainActivity.kt
@@ -8,11 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
+import com.lanazirot.pokedex.ui.navigation.AppNavGraph
+import com.lanazirot.pokedex.ui.providers.AppProvider
+import com.lanazirot.pokedex.ui.providers.GlobalProvider
 import com.lanazirot.pokedex.ui.theme.PokedexTheme
 
 
@@ -20,33 +24,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var navController = rememberNavController()
+            val gp = AppProvider(navigation = navController)
             PokedexTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                CompositionLocalProvider(
+                    GlobalProvider provides gp
                 ) {
-                    Greeting()
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        AppNavGraph(globalProvider = gp)
+                    }
+                    }
                 }
             }
         }
     }
-}
 
 
-@Composable
-fun Greeting() {
-    val painter = rememberAsyncImagePainter(model = "file:///android_asset/images/019.png")
-    Image(
-        painter = painter,
-        contentDescription = "Imagen con filtro negro",
-        colorFilter = ColorFilter.tint(Color.Black)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokedexTheme {
-        Greeting()
+    @Composable
+    fun Greeting() {
+        val painter = rememberAsyncImagePainter(model = "file:///android_asset/images/019.png")
+        Image(
+            painter = painter,
+            contentDescription = "Imagen con filtro negro",
+            colorFilter = ColorFilter.tint(Color.Black)
+        )
     }
-}
+
