9f6ef6804b8a91fefc748cfdfb0ea1cc4f50e04b
JaimeVJ-TEC <L19100265@nlaredo.tecnm.mx>
2023-03-31 13:57:47 -0500

    Graph Update

--- PokemonApp/app/src/main/java/com/tec/pokedexapp/ui/navigator/graphs/HomeGraph.kt
+++ PokemonApp/app/src/main/java/com/tec/pokedexapp/ui/navigator/graphs/HomeGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.tec.pokedexapp.ui.game.GameScreen
 import com.tec.pokedexapp.ui.navigator.main.HomeScreen
 import com.tec.pokedexapp.ui.navigator.main.PerfilScreen
 import com.tec.pokedexapp.ui.navigator.screens.BottomBarScreens
@@ -33,5 +34,7 @@ fun HomeGraph(navController: NavHostController){
         }
 
         PokeGraph(navController)
+
+        GameGraph(navController)
     }
 }
