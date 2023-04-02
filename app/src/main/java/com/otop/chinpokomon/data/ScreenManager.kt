c3ee3b7aa848cd110b83eb76ef5ea3cc7684f57f
¨RevisOTO¨ <rafamosqueda.barragan12@gmail.com>
2023-04-01 19:58:01 -0500

    Limpio

--- app/src/main/java/com/otop/chinpokomon/data/ScreenManager.kt
+++
@@ -1,7 +0,0 @@
-package com.otop.chinpokomon.data
-
-sealed class Screen(val route: String) {
-    object Home: Screen("home")
-    object Profile: Screen("profile")
-    object Settings: Screen("settings")
-}
--- app/src/main/java/com/otop/chinpokomon/data/models/Pokedex.kt
+++
@@ -1,3 +0,0 @@
-package com.otop.chinpokomon.data.models
-
-data class Pokedex (val id: Long, val idPokemon: Int,val idUser:Int)
