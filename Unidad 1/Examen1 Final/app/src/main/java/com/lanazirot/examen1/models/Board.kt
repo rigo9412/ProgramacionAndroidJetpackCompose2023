351e3a19ac9b72da0a14f7bf438e74c10eb7caf5
Alan Peña Ortiz <alancin7@live.com.mx>
2023-04-18 22:17:14 -0500

    previo examen unidad 2, reformas gato hilt dagger

--- Unidad 1/Examen1 Final/app/src/main/java/com/lanazirot/examen1/models/Board.kt
+++
@@ -1,3 +0,0 @@
-package com.lanazirot.examen1.models
-
-data class Board(var player: Char = 'X', var gatoArray: Array<Array<String>> = arrayOf( arrayOf("","",""), arrayOf("","",""), arrayOf("","","") ))
