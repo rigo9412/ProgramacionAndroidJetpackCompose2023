7b5f0bc95d01cb6e42f5813c70ad9f3d5c5c5197
ReneSalinas <renesalinas0525@gmail.com>
2023-04-21 14:37:20 -0500

    asiquedalimpio

--- PokeTest/app/src/main/java/com/rigo9412/poketest/ui/main/MainAppState.kt
+++
@@ -1,9 +0,0 @@
-package com.rigo9412.poketest.ui.main
-
-import com.rigo9412.poketest.data.model.Pokemon
-
-sealed class MainAppState {
-    object Loading: MainAppState()
-    object Error: MainAppState()
-    class Done(val pokedex: List<Pokemon>): MainAppState()
-}
