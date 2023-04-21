d3d86c00e517f8f6f7a27f2b7a0f3a3de15a3dce
Alan Peña Ortiz <alancin7@live.com.mx>
2023-04-21 11:09:20 -0500

    nose cual

--- SimonSays/SimonSays/app/src/main/java/com/lanazirot/simonsays/presentation/pad/components/CustomDialog.kt
+++ SimonSays/SimonSays/app/src/main/java/com/lanazirot/simonsays/presentation/pad/components/CustomDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
